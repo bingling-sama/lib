b1ce3ccb061247f850d76f61dc3f6b69d8cc0053
liyuning <15979829+gou-ning@user.noreply.gitee.com>
2025-11-13 21:19:33 +0800

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,33 @@
 import { useEffect } from "react"
 import { Link, Outlet, useNavigate } from "react-router-dom"
+import { Button, Flex } from "antd"
 export function Home() {
-    const navigate=useNavigate()
-    useEffect(()=>{
-        if(localStorage.getItem('token')===''){
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (localStorage.getItem('token') === '') {
             navigate('/')
-    }
+        }
     })
     return (
         <div className="left_part">
-            <ul>
+            {/* <ul>
                 <li><Link to='/book/borrow'>借阅书籍</Link></li>
                 <li><Link to='/book/search'>查找书籍</Link></li>
                 <li><Link to='/book/return'>归还书籍</Link></li>
-            </ul>
-            <Outlet />
+            </ul> */}
+            <Flex vertical gap="small" style={{ width: '100%' }}>
+                <Button  block>
+                    <Link to='/book/borrow'>借阅书籍</Link>
+                </Button>
+                <Button  block>
+                    <Link to='/book/search'>查找书籍</Link>               
+                </Button>
+                <Button  block>
+                    <Link to='/book/return'>归还书籍</Link>               
+                </Button>
+
+            </Flex>
+                            <Outlet />
         </div>
     )
 } 
